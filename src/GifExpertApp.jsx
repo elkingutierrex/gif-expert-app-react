4c3c7fce981d73aa0d71cc05e4f63d4ee1de573d
@id26072 <elkin.gutierrez@servexternos.santander.cl>
2024-08-12 17:58:36 -0500

    feat : test add category

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch']);
+
+    const onAddCategory = () => {
+        setCategories(['HunterXHunter' , ...categories ]);
+    }
+    
   return (
     <>
         <h1>GifExpertApp</h1>
+
+        <button onClick={onAddCategory}>
+            Agregar
+        </button>
+
+        <ol>
+            {
+                categories.map( category => {
+                    return <li key={category}>{category}</li>
+                })
+            }
+        </ol>
       
     </>
   )
